cbea385c4f854c885e16d9fcb843c6ab1bdb3a0d
evgeniyvlb <su160985pev@gmail.com>
2019-03-04 12:43:04 +0200

    Updates for Lesson_5

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+#git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem 'rspec'
+gem 'rspec-core'
+gem 'site_prism'
+gem 'capybara'
+gem 'capybara/dsl'
---
+++ Lib/webstrongqa.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+require 'site_prism'
+require 'capybara'
+require 'capybara/dsl'
+include Capybara::DSL
+
+Capybara.default_driver = :firefox
+Capybara.register_driver :firefox do |app|
+  options = {
+    js_errors: true,
+    timeout: 360
+  }
+  Capybara::Selenium::Driver.new(app, browser: :firefox)
+end
+
+class LoginSection < SitePrism::Section
+  element :useremail, '#user_email'
+  element :password, '#user_password'
+  element :login_button, 'input[name="commit"]'
+  element :remember_me, :xpath, '//label[@for="user_remember_me"]'
+end
+
+class Home < SitePrism::Page
+  set_url 'http://demoapp.strongqa.com'
+  element :login_btn, :xpath, '//a[contains(text(), "Login")]'
+  element :logout_btn, :xpath, '//a[contains(text(), "Logout")]'
+  element :flash_alert, '#flash_alert'
+  section :login_section, LoginSection, '.authform'
+
+  def set_useremail(useremail)
+    login_section.useremail.set useremail
+  end
+
+  def set_password(password)
+    login_section.password.set password
+  end
+
+end
